a798b9cda1c19bb0f1a0fa89cad9f22219210f65
Another-movie <legenda0601@gmail.com>
2020-04-22 17:18:07 +0600

    Added chart.js

--- Web/src/App.vue
+++ Web/src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Hello
+    <canvas id="planet-chart"></canvas>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Chart from 'chart.js';
 
+
+import planetChartData from './chart-data.js';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+        return {
+            planetChartData: planetChartData,
+        }
+    },
+    methods: {
+        createChart(chartId, chartData) {
+            const ctx = document.getElementById(chartId);
+            const myChart = new Chart(ctx, {
+            type: chartData.type,
+            data: chartData.data,
+            options: chartData.options,
+          });
+        }
+    },
+    mounted() {
+        this.createChart('planet-chart', this.planetChartData);
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
