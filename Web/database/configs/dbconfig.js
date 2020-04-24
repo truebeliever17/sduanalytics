5a1890e3174141b8450ed3aa05996cc38ecb48f3
Madiyar Mukushev <madiyar2000@gmail.com>
2020-04-24 10:13:22 +0600

    Changed scopes for security

--- Web/database/configs/dbconfig.js
+++ Web/database/configs/dbconfig.js
@@ -1,6 +1,5 @@
 module.exports = {
-  user: process.env.NODE_ORACLEDB_USER || "hr",
-  password: process.env.NODE_ORACLEDB_PASSWORD,
-  connectString:
-    process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/orcldb",
+  user: process.env.HR_USER,
+  password: process.env.HR_PASSWORD,
+  connectString: process.env.HR_CONNECTIONSTRING,
 };
