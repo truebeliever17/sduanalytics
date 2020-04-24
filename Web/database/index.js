c2ff6ffff09ee49945cdd0c17eba0c95634781bd
Madiyar Mukushev <madiyar2000@gmail.com>
2020-04-24 10:12:02 +0600

    Created connection between database

--- Web/database/index.js
+++ Web/database/index.js
@@ -1,35 +1,77 @@
-const oracledb = require('oracledb');
-const dbConfig = require('./dbconfig.js');
+const oracledb = require("oracledb");
+const dbConfig = require("./configs/dbconfig.js");
+const fs = require("fs");
+const path = require("path");
 
-async function init() {
+async function run() {
+  let pool;
+
+  try {
+    pool = await oracledb.createPool(dbConfig);
+
+    let connection;
     try {
-        // Create a connection pool which will later be accessed via the
-        // pool cache as the 'default' pool.
-        await oracledb.createPool({
-            user: dbConfig.user,
-            password: dbConfig.password,
-            connectString: dbConfig.connectString
-            // edition: 'ORA$BASE', // used for Edition Based Redefintion
-            // events: false, // whether to handle Oracle Database FAN and RLB events or support CQN
-            // externalAuth: false, // whether connections should be established using External Authentication
-            // homogeneous: true, // all connections in the pool have the same credentials
-            // poolAlias: 'default', // set an alias to allow access to the pool via a name.
-            // poolIncrement: 1, // only grow the pool by one connection at a time
-            // poolMax: 4, // maximum size of the pool. Increase UV_THREADPOOL_SIZE if you increase poolMax
-            // poolMin: 0, // start with no connections; let the pool shrink completely
-            // poolPingInterval: 60, // check aliveness of connection if idle in the pool for 60 seconds
-            // poolTimeout: 60, // terminate connections that are idle in the pool for 60 seconds
-            // queueMax: 500, // don't allow more than 500 unsatisfied getConnection() calls in the pool queue
-            // queueTimeout: 60000, // terminate getConnection() calls queued for longer than 60000 milliseconds
-            // sessionCallback: myFunction, // function invoked for brand new connections or by a connection tag mismatch
-            // stmtCacheSize: 30 // number of statements that are cached in the statement cache of each connection
-        });
-        console.log('Connection pool started');
-        await dostuff();
-
+      connection = await pool.getConnection();
+      let queries = [
+        [
+          "SELECT * FROM TABLE(find_popular_courses(:year, :term))",
+          `SELECT DISTINCT year, term FROM course_selections`,
+          "popular_courses.json",
+        ],
+        [
+          "SELECT * FROM TABLE(find_popular_teachers(:ders_kod, :year, :term))",
+          `SELECT DISTINCT ders_kod, year, term FROM course_selections`,
+          "popular_teachers.json",
+        ],
+        [
+          "SELECT * FROM TABLE(find_spa_and_gpa(:stud_id, :year, :term))",
+          `SELECT DISTINCT stud_id, year, term FROM course_selections`,
+          "gpa.json",
+        ],
+        [
+          "SELECT * FROM TABLE(find_nonregistered_students(:year, :term))",
+          `SELECT DISTINCT year, term FROM course_selections`,
+          "nonregistered_students.json",
+        ],
+      ];
+      for (query_set of queries) {
+        let permutations = await connection.execute(query_set[1], []);
+        let arr = [];
+        console.log(permutations.rows);
+        for (instance of permutations.rows) {
+          let query_result = await connection.execute(query_set[0], instance, {
+            outFormat: oracledb.OUT_FORMAT_OBJECT,
+          });
+          arr = arr.concat(query_result.rows);
+        }
+        let json = JSON.stringify(arr);
+        fs.writeFile(
+          path.join(__dirname, "./json/" + query_set[2]),
+          json,
+          "utf8",
+          function(err) {
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
+      }
     } catch (err) {
-        console.error('init() error: ' + err.message);
+      throw err;
     } finally {
-        await closePoolAndExit();
+      if (connection) {
+        try {
+          await connection.close();
+        } catch (err) {
+          throw err;
+        }
+      }
     }
+  } catch (err) {
+    console.error(err.message);
+  } finally {
+    await pool.close(0);
+  }
 }
+
+run();
