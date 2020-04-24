4060d458762e6648eca97edd5cefd8ccd28ef7be
Madiyar Mukushev <madiyar2000@gmail.com>
2020-04-24 16:46:08 +0600

    More queries added

--- Web/database/index.js
+++ Web/database/index.js
@@ -33,16 +33,77 @@ async function run() {
           `SELECT DISTINCT year, term FROM course_selections`,
           "nonregistered_students.json",
         ],
+        [
+          "SELECT * FROM TABLE(calculate_retake_expenses(:stud_id, :year, :term))",
+          `SELECT DISTINCT stud_id, year, term FROM course_selections`,
+          "retake_expenses.json",
+        ],
+        [
+          "SELECT * FROM TABLE(calculate_teacher_loading(:emp_id, :year, :term))",
+          `SELECT DISTINCT emp_id, year, term FROM course_sections`,
+          "teachers_loading.json",
+        ],
+        [
+          "SELECT * FROM TABLE(create_student_schedule(:stud_id, :year, :term))",
+          `SELECT DISTINCT stud_id, year, term FROM course_selections`,
+          "students_schedule.json",
+        ],
+        [
+          "SELECT * FROM TABLE(create_teacher_schedule(:emp_id, :year, :term))",
+          `SELECT DISTINCT emp_id, year, term FROM course_sections`,
+          "teachers_schedule.json",
+        ],
+        [
+          "SELECT * FROM TABLE(calculate_number_of_subjects(:stud_id, :year, :term))",
+          `SELECT DISTINCT stud_id, year, term FROM course_selections`,
+          "students_number_of_subjects.json",
+        ],
+        [
+          "SELECT * FROM TABLE(find_most_clever_flow(:emp_id, :ders_kod, :year, :term))",
+          `SELECT DISTINCT emp_id, ders_kod, year, term FROM course_sections WHERE emp_id IS NOT NULL`,
+          "most_clever_flows.json",
+        ],
+        [
+          "SELECT * FROM TABLE(find_teachers_rating(:year, :term))",
+          `SELECT DISTINCT year, term FROM course_sections`,
+          "teachers_rating.json",
+        ],
+        [
+          "SELECT * FROM TABLE(find_courses_rating(:year, :term))",
+          `SELECT DISTINCT year, term FROM course_selections`,
+          "courses_rating.json",
+        ],
+        [
+          "SELECT * FROM TABLE(calculate_all_retakes())",
+          "",
+          "all_retakes_info.json",
+        ],
       ];
+
       for (query_set of queries) {
-        let permutations = await connection.execute(query_set[1], []);
         let arr = [];
-        console.log(permutations.rows);
-        for (instance of permutations.rows) {
-          let query_result = await connection.execute(query_set[0], instance, {
-            outFormat: oracledb.OUT_FORMAT_OBJECT,
-          });
-          arr = arr.concat(query_result.rows);
+        if (query_set[1].length < 1) {
+          let query_result = await connection.execute(
+            query_set[0],
+            {},
+            {
+              outFormat: oracledb.OUT_FORMAT_OBJECT,
+            }
+          );
+          arr = query_result.rows;
+        } else {
+          let permutations = await connection.execute(query_set[1], []);
+          console.log(permutations.rows);
+          for (instance of permutations.rows) {
+            let query_result = await connection.execute(
+              query_set[0],
+              instance,
+              {
+                outFormat: oracledb.OUT_FORMAT_OBJECT,
+              }
+            );
+            arr = arr.concat(query_result.rows);
+          }
         }
         let json = JSON.stringify(arr);
         fs.writeFile(
