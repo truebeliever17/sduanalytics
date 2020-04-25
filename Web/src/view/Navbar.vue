21cd746ccd2678fb946e5ab41ac7cc41568640b8
Madiyar Mukushev <madiyar2000@gmail.com>
2020-04-25 14:57:53 +0600

    Added navbar to main page

---
+++ Web/src/view/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <b-navbar navbar-background-color="#000000">
+    <template slot="brand">
+      <b-navbar-item>
+        <img src="../../images/logo.png" />
+      </b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item href="#">
+        Home
+      </b-navbar-item>
+      <b-navbar-item href="#">
+        Documentation
+      </b-navbar-item>
+    </template>
+
+    <template slot="end">
+        <sidebar></sidebar>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+import sidebar from "./Sidebar.vue";
+export default {
+  components: {
+    sidebar,
+  },
+};
+</script>
